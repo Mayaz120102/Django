c041ed60bbdd0eec1b17ba03fdfdb0ae3b9b0f92
Mayaz120102 <abrarmayaz2002@gmail.com>
2024-10-10 17:32:56 +0600

    display

--- self_learning/flagman/urls.py
+++ self_learning/flagman/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.main, name = 'main'),
     path('flagman/', views.flagman, name = 'flagman'),
+    path('flagman/details/<int:id>', views.details, name ='details'),
+    path('testing/', views.testing, name='testing'),
 ]
